ab240358f8af7623aa12ec1907bd43ac6138e674
Theodore Smith <tjs7321@gmail.com>
2023-07-15 06:27:33 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const screenText = document.getElementById("text")
+    screenText.textContent = "This is really cool!"
+})
